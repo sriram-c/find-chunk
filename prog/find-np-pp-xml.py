7f3012926e39f223db2c5b590f7da92b6f3e6a8c
sriram <sriramchaudhury@gmail.com>
2020-10-25 18:34:11 +0530

    updated prog to find uniq ids for node

--- prog/find-np-pp-xml.py
+++ prog/find-np-pp-xml.py
@@ -6,25 +6,23 @@ from bs4 import BeautifulSoup, Tag, NavigableString
 
 def find_leaf(tag, phrase):
     tmp_wd = []
+    tmp_id = []
 
     if(tag['value'] == 'CC'):
         cc_wd = tag.find_all('leaf')[0]['value']
 
         prev_sib = list(tag.previous_siblings)[1]
-        prev_sib_depth = str(len(list(prev_sib.parents)))
-        prev_sib1 = prev_sib['value']+prev_sib_depth
+        prev_sib1 = prev_sib['value']+str(prev_sib['id'])
 
         next_sib = list(tag.next_siblings)[1]
-        next_sib_depth = str(len(list(next_sib.parents)))
-        next_sib1 = next_sib['value'] + next_sib_depth
+        next_sib1 = next_sib['value'] + str(next_sib['id'])
 
-        phrase.append([tag['value']+str(len(list(tag.parents))), prev_sib1, next_sib1, cc_wd])
+        phrase.append([tag['value']+str(tag['id']), prev_sib1, next_sib1, cc_wd])
 
-    #elif (tag.name != 'leaf'):
     elif (re.match( r'^NP|WHNP|PP|WHPP|ADJP|WHADJP|ADVP|WHAVP|X|SBAR|NAC|NML|CONJP|FRAG|INTJ|LST|NAC|NX|QP|PRC|PRN|PRT|QP|RRC|UCP|ROOT|S|,$', tag['value']) and (tag.name != 'leaf')):
         for wd in tag.find_all('leaf'):
                 tmp_wd.append(wd['value']+'_'+str(wd['id']))
-        phrase.append([tag['value']+str(len(list(tag.parents))), tmp_wd])
+        phrase.append([tag['value']+str(tag['id']), tmp_wd])
 
 def find_lwg(tag,tmp_lwg,flag):
     for tag1 in tag.contents:
@@ -32,20 +30,18 @@ def find_lwg(tag,tmp_lwg,flag):
             if (re.match(r'^VB|RB|TO|MD', tag1['value'])):
                 tmp_val = tag1.find_all('leaf')[0]['value']
                 tmp_id = tag1.find_all('leaf')[0]['id']
-                #tmp_lwg.append(tag1.find_all('leaf')[0]['value'])
                 tmp_lwg.append(tmp_val+'_'+str(tmp_id))
 
             elif (re.match(r'^VP', tag1['value'])):
                 find_lwg(tag1,tmp_lwg,1)
     if(flag == 0):
-        phrase.append([tag['value']+'_LWG'+str(len(list(tag.parents))), tmp_lwg])
+        phrase.append([tag['value']+str(tag['id'])+'_LWG', tmp_lwg])
 
 
 def process_tag(tag, phrase):
         if (isinstance(tag, Tag)):
             if (tag['value'] == 'VP') and (tag.parent['value'] != 'VP'):
                 tmp_lwg = []
-                #find_leaf(tag)
                 find_lwg(tag,tmp_lwg,0)
                 for tag1 in tag.contents:
                     if (isinstance(tag1, Tag)):
@@ -62,9 +58,6 @@ def process_tag(tag, phrase):
                         process_tag(tag1, phrase)
 
 
-
-
-
 with open(sys.argv[1]) as fp:
     soup = BeautifulSoup(fp, 'lxml-xml')
 
@@ -76,6 +69,10 @@ for node in root.find_all('leaf'):
     node['id'] = id
     id += 1
 
+id = 1
+for node in root.find_all('node'):
+    node['id'] = id
+    id += 1
 
 
 phrase = []
@@ -83,8 +80,18 @@ for ch in root.contents:
     if(isinstance(ch, Tag)):
         process_tag(ch,phrase)
 
+phrase_dic = {}
+count = 1
 for p in phrase:
     if(re.match(r'CC',p[0])):
-        print(p[0], ':', p[1],'-', p[3], '-', p[2])
+        phrase_dic[p[0]] = p[1]+'-'+p[3]+'-'+p[2]
     else:
-        print(p[0],':',' '.join(p[1]))
+        if p[0] in phrase_dic:
+            new_id = p[0]+'_'+str(count)
+            count += 1
+            phrase_dic[new_id] = ' '.join(p[1])
+        else:
+            phrase_dic[p[0]] = ' '.join(p[1])
+
+for key in phrase_dic:
+    print(key,phrase_dic[key])
